938da522277c2963f082a58ed52710df8f51ee72
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2021-02-07 15:05:34 +0200

    Add files via upload

--- Dijital saat.cpp
+++ Dijital saat.cpp
@@ -2,17 +2,17 @@
 #include <windows.h>
 int main()
 {
-int i,j,l;
-for(i=0;i<24;i++)
-{
-for(j=0;j<60;j++)
-{
-for(l=0;l<60;l++)
-{
-printf("%02d:%02d:%02d\n",i,j,l);
-Sleep(1000);
-}
-}
-}
+int i,j,k;
+	for(i=0;i<24;i++)
+	{
+	 for(j=0;j<60;j++)
+	 {
+ 	   for(k=0;k<60;k++)
+	   {
+	     printf("%02d:%02d:%02d\n",i,j,k);
+	 Sleep(1000);
+       }
+     }
+   }
 return 0;
 }
